f57ac46d5306e8fa4755767e6c2c968f8f18515c
garima24012007 <garimayadav2412007@gmail.com>
2026-06-01 19:02:54 +0530

    finished--DAY-1

---
+++ DAY--1/CODE-3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,fact;
+    cout<<"enter the number :";
+    cin>>n;
+    fact=1;
+    for(i=1;i<=n;i++){
+     fact=fact*i;
+     }
+    cout<<fact;
+    return 0;
+}
